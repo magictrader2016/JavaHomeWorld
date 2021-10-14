c67f1cc71488d4204709b8891049339e5242ae01
magictrader2016 <magictrader2016@gmail.com>
2021-10-14 20:08:32 +0300

    init commit

--- src/com/pb/magictrader/HW2/Calculator.java
+++ src/com/pb/magictrader/HW2/Calculator.java
@@ -1,4 +1,21 @@
 package com.pb.magictrader.HW2;
 
+import java.util.Scanner;
+
 public class Calculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        int y;
+        String symbol;
+
+        x = scan.nextInt();
+        y = scan.nextInt();
+        symbol = scan.nextLine();
+
+        System.out.println("x = " + x + " y= " + y + " symbol = " + symbol);
+    }
 }
+
+
+
