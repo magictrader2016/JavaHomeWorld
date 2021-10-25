9ebcdd33858c05cba416b7e6d75f474b9171be29
magictrader2016 <92296911+magictrader2016@users.noreply.github.com>
2021-10-25 15:27:46 +0300

    init commit

--- src/com/pb/magictrader/HW4/Capital1.java
+++ src/com/pb/magictrader/HW4/Capital1.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class Capital1 {
     private static Scanner sc;
     public static void main(String[] args) {
-        System.out.println("ENTER TEXT:");
-        Scanner scan = new Scanner(System.in);
-        int number = scan.nextInt();
-        String[] tests = scan.nextInt();
+        Scanner in = new Scanner(System.in);
+        System.out.print("ENTER PHRASE: ");
+        String first = in.nextLine().toUpperCase();
+        String[] tests = {"XXXX"};
 
         for (String s : tests) {
             System.out.println(toUp(s));
